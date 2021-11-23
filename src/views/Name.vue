07049cb24c0c100aab57fa758a6aecd479575da8
Casey Hill <bignamehere@gmail.com>
2021-11-23 17:42:35 -0600

    updated Router example [appends #24]

--- src/views/Name.vue
+++ src/views/Name.vue
@@ -6,10 +6,10 @@
     <main id="main-content" tabindex="-1">
       <div class="fsa-section">
         <div class="fsa-section__bd">
-          <h1>What is your name?</h1>
+          <h1>Dynamic Router Demo</h1>
 
           <div class="fsa-m-t--sm">
-              <p class="fsa-m-t--7">Cool Name...  <span class="fsa-text-size--5">{{ passedName }}</span></p>
+              <p class="fsa-m-t--7">What page are we on?  <span class="fsa-text-size--5">{{ pageName }}</span></p>
           </div>
 
           
@@ -40,10 +40,10 @@ export default {
     
     const route = useRoute();
 
-    const passedName = route.params.id;
+    const pageName = route.params.id;
 
     return {
-      passedName
+      pageName
     }
   }
 }
