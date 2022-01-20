4d08a0af80113de545ccff0cdd9307f77b464534
Casey Hill <bignamehere@gmail.com>
2022-01-20 12:47:16 -0600

    Updates

--- assets/NotFound.ceb3d95b.js
+++
@@ -1,1 +0,0 @@
-import{o as e,c as o}from"./vendor.98e3be04.js";const n={setup(){}};n.render=function(n,r,t,s,u,a){return e(),o("h1",null,"Page Not Found")};export{n as default};
---
+++ assets/useNavigation.1f355f07.js
@@ -0,0 +1,1 @@
+import{p as s}from"./vendor.28259ad5.js";function t(){const n=s();return{goto:(o,e="")=>{o.includes("http")?e=="_blank"?window.open(o,e):window.location.href=o:n.push(o)}}}export{t as u};
