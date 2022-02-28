b441010f58fa4645fc247687ff18bbdf42c8ec46
Casey Hill <bignamehere@gmail.com>
2022-02-28 11:46:39 -0600

    Updates

--- assets/NotFound.73a182ae.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./index.b5a23908.js";import{o as e,f as t}from"./vendor.054b4197.js";const r={setup(){}};function n(s,a,c,p,_,d){return e(),t("h1",null,"Page Not Found")}var i=o(r,[["render",n]]);export{i as default};
